1479649b4be2e8f3dd87d821a4ee5af547563c15
Louis Musole <l88mus@gmail.com>
2023-05-06 11:30:42 +0200

    Final stage stateflow

--- app/src/main/java/com/drcmind/androidasynchroniouswithflow/Result.kt
+++ app/src/main/java/com/drcmind/androidasynchroniouswithflow/Result.kt
@@ -1,6 +1,6 @@
 package com.drcmind.androidasynchroniouswithflow
 
-sealed  class Result<T>(){
+sealed  class Result<T>{
     object Loading : Result<Nothing>()
     data class Success<T>(val data : T) : Result<T>()
 }
